ea2badbe0759917ac70664b3bb1efae89f789381
Geraldo de Lima Junior <geraldo.lima.junior@ccc.ufcg.edu.br>
2024-08-31 22:59:09 -0300

    fixes some errors

--- app/JogoDaVelha.hs
+++ app/JogoDaVelha.hs
@@ -2,7 +2,7 @@ module JogoDaVelha where
 import Control.Monad (replicateM_)
 import Data.Map (Map)
 import Data.Text (Text)
-import Data.Char (toLower)
+import Data.Char (toLower, isDigit)
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import Data.List (intercalate)
@@ -44,12 +44,12 @@ bola = [
 
 xis :: [String]
 xis = [
-    "     \\\\    //    ",
-    "      \\\\  //     ",
-    "       \\\\//      ",
-    "       //\\\\      ",
-    "      //  \\\\     ",
-    "     //    \\\\    "
+    "    \\\\    //     ",
+    "     \\\\  //      ",
+    "      \\\\//       ",
+    "      //\\\\       ",
+    "     //  \\\\      ",
+    "    //    \\\\     "
     ]
 
 vazio :: [String]
@@ -72,14 +72,20 @@ pegaPartesDoTabuleiro n xs = take n xs : pegaPartesDoTabuleiro n (drop n xs)
 
 printaPartes :: [[[String]]] -> IO ()
 printaPartes [] = return ()
+printaPartes [x] = mapM_ (putStrLn . intercalate " | ") (formata x)
 printaPartes (x:xs) = do
     mapM_ (putStrLn . intercalate " | ") (formata x)
-    putStrLn (replicate (length (head (head x)) * 3 + 2) '-')
+    let tileWidth = length (head (head x))
+    let lineLength = tileWidth * 3 + 2 * 2 
+    putStrLn (replicate lineLength '-')
     printaPartes xs
 
-formata :: [[a]] -> [[a]]
-formata ([]:_) = []
-formata x = map head x : formata (map tail x)
+formata :: [[String]] -> [[String]]
+formata = transpose
+
+transpose :: [[a]] -> [[a]]
+transpose ([]:_) = []
+transpose x = map head x : transpose (map tail x)
 
 atualizaTabuleiro :: [[String]] -> Char -> Int -> [[String]]
 atualizaTabuleiro tabuleiro jogador pos = take (pos - 1) tabuleiro ++ [bloco] ++ drop pos tabuleiro
@@ -112,25 +118,41 @@ checaEmpate = notElem vazio
 
 pegaInput :: [[String]] -> Char -> IO (Char, Int)
 pegaInput tabuleiro jogador = do
-    putStrLn $ "jogador " ++ [jogador] ++ ", enter your move (format: 1-9):"
+    putStrLn $ "Vez do jogador " ++ [jogador] ++ ". Insira seu movimento (format 1-9):"
     input <- getLine
-    let pos = read input :: Int
-    return (jogador, pos)
+    if length input /= 1 || not (isDigit (head input)) then do
+        putStrLn "Entrada inválida. Por favor, insira um número entre 1 e 9."
+        pegaInput tabuleiro jogador
+    else
+        let pos = read input :: Int
+        in if pos < 1 || pos > 9 then do
+            putStrLn "Entrada inválida. Por favor, insira um número entre 1 e 9."
+            pegaInput tabuleiro jogador
+        else if isTileOccupied tabuleiro pos then do
+            putStrLn "Posição já ocupada. Por favor, escolha outra posição."
+            pegaInput tabuleiro jogador
+        else
+            return (jogador, pos)
+
+isTileOccupied :: [[String]] -> Int -> Bool
+isTileOccupied tabuleiro pos = let row = (pos - 1) `div` 3
+                                   col = (pos - 1) `mod` 3
+                               in tabuleiro !! (pos - 1) /= vazio
 
 gameLoop :: [[String]] -> Char -> IO ()
 gameLoop tabuleiro jogador = do
-    let chunks = pegaPartesDoTabuleiro 3 tabuleiro
-    printaPartes chunks
+    let partes = pegaPartesDoTabuleiro 3 tabuleiro
+    printaPartes partes
     (jogadorAtual, pos) <- pegaInput tabuleiro jogador
     let updatedtabuleiro = atualizaTabuleiro tabuleiro jogadorAtual pos
     if checaVitoria updatedtabuleiro jogadorAtual
         then do
-            let updatedChunks = pegaPartesDoTabuleiro 3 updatedtabuleiro
-            printaPartes updatedChunks
-            putStrLn $ "jogador " ++ [jogadorAtual] ++ " wins!"
+            let partesAtualizadas = pegaPartesDoTabuleiro 3 updatedtabuleiro
+            printaPartes partesAtualizadas
+            putStrLn $ "Jogador " ++ [jogadorAtual] ++ " vence!"
         else if checaEmpate updatedtabuleiro
             then do
-                let updatedChunks = pegaPartesDoTabuleiro 3 updatedtabuleiro
-                printaPartes updatedChunks
-                putStrLn "It's a draw!"
+                let partesAtualizadas = pegaPartesDoTabuleiro 3 updatedtabuleiro
+                printaPartes partesAtualizadas
+                putStrLn "Temos um empate!"
             else gameLoop updatedtabuleiro (if jogador == 'X' then 'O' else 'X')
