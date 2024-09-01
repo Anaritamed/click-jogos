b5826f450b8005dbabda9df2be480d3721903ffc
Vinicius-TML <viniciustmlopes@hotmail.com>
2024-09-01 00:03:32 -0300

    Jogo da Velha Finalizado

--- app/JogoDaVelha.hs
+++ app/JogoDaVelha.hs
@@ -1,16 +1,18 @@
 module JogoDaVelha where
 import Control.Monad (replicateM_)
+
 import Data.Map (Map)
 import Data.Text (Text)
 import Data.Char (toLower, isDigit)
+import Utils (limpaTerminal)
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import Data.List (intercalate)
-import Distribution.ModuleName (main)
-
+import Control.Concurrent (threadDelay)
 
 jogoDaVelha :: IO()
 jogoDaVelha = do
+    limpaTerminal
     putStrLn "============================================================================"
     putStrLn "      _                         _        __     __   _ _                    "
     putStrLn "     | | ___   __ _  ___     __| | __ _  \\ \\   / /__| | |__   __ _        "
@@ -20,13 +22,13 @@ jogoDaVelha = do
     putStrLn "              |___/                                                         "
     putStrLn "============================================================================"
     putStrLn "                 SEJA BEM VINDO!                                            "
-    putStrLn"                                                                             "
-    putStrLn"                 (1) JOGAR                                                   "
-    putStrLn"                 (2) RETORNAR AO MENU                                        "
-    putStrLn"                                                                             "
+    putStrLn "                                                                             "
+    putStrLn "                 (1) JOGAR                                                   "
+    putStrLn "                 (2) RETORNAR AO MENU                                        "
+    putStrLn "                                                                             "
     opcao <- getLine
     if opcao == "1" then do
-        main
+        iniciarJogo
     else if opcao == "2" then do
         putStrLn "tem que voltar ao menu"
     else do
@@ -76,7 +78,7 @@ printaPartes [x] = mapM_ (putStrLn . intercalate " | ") (formata x)
 printaPartes (x:xs) = do
     mapM_ (putStrLn . intercalate " | ") (formata x)
     let tileWidth = length (head (head x))
-    let lineLength = tileWidth * 3 + 2 * 2 
+    let lineLength = tileWidth * 3 + 2 * 2 -- 3 tiles and 2 separators between them
     putStrLn (replicate lineLength '-')
     printaPartes xs
 
@@ -113,12 +115,23 @@ checaVitoria tabuleiro jogador = any (all (== bloco)) padroesVitoria
         [tabuleiro !! 2, tabuleiro !! 4, tabuleiro !! 6]
         ]
 
+reiniciaJogo :: IO()
+reiniciaJogo = do
+    putStrLn"Reiniciando o jogo em"
+    putStrLn"3"
+    threadDelay (1 * 1000000)
+    putStrLn "2"
+    threadDelay (1 * 1000000)
+    putStrLn "1"
+    threadDelay (1 * 1000000)
+    jogoDaVelha
+
 checaEmpate :: [[String]] -> Bool
 checaEmpate = notElem vazio
 
 pegaInput :: [[String]] -> Char -> IO (Char, Int)
 pegaInput tabuleiro jogador = do
-    putStrLn $ "Vez do jogador " ++ [jogador] ++ ". Insira seu movimento (format 1-9):"
+    putStrLn $ "Vez do jogador " ++ [jogador] ++ ". Insira seu movimento (posições 1-9):"
     input <- getLine
     if length input /= 1 || not (isDigit (head input)) then do
         putStrLn "Entrada inválida. Por favor, insira um número entre 1 e 9."
@@ -128,14 +141,14 @@ pegaInput tabuleiro jogador = do
         in if pos < 1 || pos > 9 then do
             putStrLn "Entrada inválida. Por favor, insira um número entre 1 e 9."
             pegaInput tabuleiro jogador
-        else if isTileOccupied tabuleiro pos then do
+        else if posicaoOcupada tabuleiro pos then do
             putStrLn "Posição já ocupada. Por favor, escolha outra posição."
             pegaInput tabuleiro jogador
         else
             return (jogador, pos)
 
-isTileOccupied :: [[String]] -> Int -> Bool
-isTileOccupied tabuleiro pos = let row = (pos - 1) `div` 3
+posicaoOcupada :: [[String]] -> Int -> Bool
+posicaoOcupada tabuleiro pos = let row = (pos - 1) `div` 3
                                    col = (pos - 1) `mod` 3
                                in tabuleiro !! (pos - 1) /= vazio
 
@@ -144,15 +157,21 @@ gameLoop tabuleiro jogador = do
     let partes = pegaPartesDoTabuleiro 3 tabuleiro
     printaPartes partes
     (jogadorAtual, pos) <- pegaInput tabuleiro jogador
-    let updatedtabuleiro = atualizaTabuleiro tabuleiro jogadorAtual pos
-    if checaVitoria updatedtabuleiro jogadorAtual
+    limpaTerminal
+    let tabuleiroAtualizado = atualizaTabuleiro tabuleiro jogadorAtual pos
+    if checaVitoria tabuleiroAtualizado jogadorAtual
         then do
-            let partesAtualizadas = pegaPartesDoTabuleiro 3 updatedtabuleiro
+            let partesAtualizadas = pegaPartesDoTabuleiro 3 tabuleiroAtualizado
             printaPartes partesAtualizadas
             putStrLn $ "Jogador " ++ [jogadorAtual] ++ " vence!"
-        else if checaEmpate updatedtabuleiro
+            reiniciaJogo
+        else if checaEmpate tabuleiroAtualizado
             then do
-                let partesAtualizadas = pegaPartesDoTabuleiro 3 updatedtabuleiro
+                let partesAtualizadas = pegaPartesDoTabuleiro 3 tabuleiroAtualizado
                 printaPartes partesAtualizadas
                 putStrLn "Temos um empate!"
-            else gameLoop updatedtabuleiro (if jogador == 'X' then 'O' else 'X')
+                reiniciaJogo
+            else gameLoop tabuleiroAtualizado (if jogador == 'X' then 'O' else 'X')
+
+iniciarJogo :: IO ()
+iniciarJogo = gameLoop arrayFinal 'X'
