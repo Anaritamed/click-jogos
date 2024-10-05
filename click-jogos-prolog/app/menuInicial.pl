f0638f0d2c86656660299ce50d5de20b0146afce
Naiara Medeiros <naiaramc.almeida@gmail.com>
2024-10-05 11:34:19 -0300

    finaliza menu inicial

---
+++ click-jogos-prolog/app/menuInicial.pl
@@ -0,0 +1,68 @@
+:- use_module(perguntados).
+
+exibe_tela_inicial :-
+    tela_inicial(Lines),
+    maplist(writeln, Lines).
+
+exibe_menu :-
+    jogos(Lines),
+    maplist(writeln, Lines).
+    
+inicio :- 
+    exibe_tela_inicial,
+    write("Digite uma opcao: "),
+    read(Opcao),
+    processaOpcaoInicio(Opcao).
+
+processaOpcaoInicio(1) :- menu.
+processaOpcaoInicio(2) :- sair.
+processaOpcaoInicio(_) :- 
+    write("Opcao invalida. Tente novamente.\n"),
+    inicio.
+
+sair :- 
+    write("Saindo..."), 
+    !.
+
+menu :- 
+    exibe_menu,
+    write("Digite uma opcao: "),
+    read(Opcao),
+    processaOpcaoMenu(Opcao).
+
+% processaOpcaoMenu(1) :- forca.
+processaOpcaoMenu(2) :- perguntados.
+% processaOpcaoMenu(3) :- jogoDaVelha.
+processaOpcaoMenu(_) :- 
+    write("Opcao invalida. Tente novamente.\n"),
+    menu.
+
+% Definição da tela inicial como lista de strings
+tela_inicial([
+    "===================================================",
+    "   ___ _ _      _       __                         ",
+    "  / __\\ (_) ___| | __   \\ \\  ___   __ _  ___  ___ ",
+    " / /  | | |/ __| |/ /    \\ \\/ _ \\ / _` |/ _ \\/ __|",
+    "/ /___| | | (__|   <  /\\_/ / (_) | (_| | (_) \\__ \\",
+    "\\____/|_|_|\\___|_|\\_\\ \\___/ \\___/ \\__, |\\___/|___/",
+    "                                  |___/            ",
+    "===================================================",
+    "          MENU (1)        |        SAIR (2)        ",
+    "===================================================",
+    "                                                   "
+]).
+
+% Definição da tela de escolha de jogos como lista de strings
+jogos([
+    "                                                   ",
+    "===================================================",
+    "                ESCOLHA UM JOGO                    ",
+    "===================================================",
+    "                    FORCA (1)                      ",
+    "                 PERGUNTADOS (2)                   ",
+    "                JOGO DA VELHA (3)                  ",
+    "===================================================",
+    "                                                   "
+]).
+
+:-inicio.
---
+++ click-jogos-prolog/app/perguntados.pl
@@ -0,0 +1,6 @@
+:- module(perguntados, [perguntados/0]).
+
+perguntados :- 
+    write("Você está jogando Perguntados!"), nl,
+    % Adicione aqui a lógica do jogo Perguntados.
+    write("Fim do jogo Perguntados.").
