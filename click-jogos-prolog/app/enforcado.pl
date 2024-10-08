3a1c95a0a88d3980da03dec2ca61645c13c2d102
anarita <ana.souza.ext@synchro.com.br>
2024-10-07 23:24:02 -0300

    chore: jogo em progresso...

--- click-jogos-prolog/app/enforcado.pl
+++ click-jogos-prolog/app/enforcado.pl
@@ -1,10 +1,16 @@
 :- module(enforcado, [
     forca/0,
     get_Entrada/3,
-    eh_valido/2
+    eh_valido/2,
+    jogo/2,
+    loop/4,
+    cria_string_sublinhados/2,
+    atualiza_string_sublinhados/4,
+    atualiza_forca/1
 ]).
 
 :- use_module(utils).
+:- use_module(library(lists)).
 
 forca :- 
     inicio.
@@ -39,10 +45,9 @@ get_dados_partida :-
     read_line_to_string(user_input, _),
     format("\nCerto ~s, qual a palavra a ser adivinhada?\n", [Jogador1]),
     get_Entrada(_, Palavra, "Palavra inválida!"),
-    writeln(Palavra),
     write("Qual o tema da palavra?\n"),
     get_Entrada(_, Tema, "Tema inválido!"),
-    writeln(Tema).
+    jogo(Palavra, Tema).
 
 regras_do_jogo(Regras, RegrasEstilizadas) :-
     Regras = "\n📜 Regras do jogo: \n- O jogador 1 será o jogador que dirá a palavra para ser adivinhada, assim como qual tema ela se relaciona.
@@ -71,4 +76,156 @@ eh_valido(_, Texto) :-
 eh_valido("letra", Tema) :- 
     string(Tema),
     string_length(Tema, Tamanho),
-    Tamanho > 0.
+    Tamanho > 0,
+    Tamanho == 1.
+
+% inicializa o jogo
+jogo(Palavra, Tema) :-
+    string_lower(Palavra, PalavraMin),
+    atom_chars(PalavraMin, ListaPalavra),
+    length(ListaPalavra, N),
+    cria_string_sublinhados(N, EstadoAtual),
+    writeln("Tema: "),
+    writeln(Tema),
+    loop(ListaPalavra, EstadoAtual, [], 0).
+
+% Função principal de loop
+loop(Palavra, EstadoAtual, LetrasDigitadas, Erros) :-
+    Erros < 6,
+    atualiza_forca(Erros),
+    format("Palavra atual: ~w~n", [EstadoAtual]),
+    format("Letras digitadas: ~w~n", [LetrasDigitadas]),
+    writeln("Digite uma letra:"),
+    read_line_to_string(user_input, Letra),
+    string_lower(Letra, LetraMin),
+    atom_chars(LetraMin, [LetraChar]),
+
+    (member(LetraChar, LetrasDigitadas) ->
+        writeln("Essa letra já foi digitada!"),
+        loop(Palavra, EstadoAtual, LetrasDigitadas, Erros)
+    ;
+        atualiza_jogo(Palavra, LetraChar, EstadoAtual, NovoEstado, Erros, NovosErros),
+        (NovoEstado == Palavra ->
+            Resultado = cenaVitoria(Palavra),
+            write(Resultado),
+            sair
+        ;
+            loop(Palavra, NovoEstado, [LetraChar|LetrasDigitadas], NovosErros)
+        )
+    ).
+
+% Atualiza o estado do jogo com a letra correta ou aumenta o contador de erros
+atualiza_jogo(Palavra, Letra, EstadoAtual, NovoEstado, Erros, NovosErros) :-
+    (ocorrencias(Palavra, Letra, Posicoes) ->
+        atualiza_string_sublinhados(Posicoes, Letra, EstadoAtual, NovoEstado),
+        NovosErros is Erros
+    ;
+        NovoEstado = EstadoAtual,
+        NovosErros is Erros + 1
+    ).
+
+% Verifica as posições da letra na palavra
+ocorrencias(Palavra, Letra, Posicoes) :-
+    findall(Index, nth1(Index, Palavra, Letra), Posicoes),
+    Posicoes \= [].
+
+% Atualiza a string sublinhada com as letras corretas
+atualiza_string_sublinhados([], _, EstadoAtual, EstadoAtual).
+atualiza_string_sublinhados([Pos|Posicoes], Letra, EstadoAtual, NovoEstado) :-
+    nth1(Pos, NovoEstado, Letra, EstadoAtualIntermediario),
+    atualiza_string_sublinhados(Posicoes, Letra, EstadoAtualIntermediario, NovoEstado).
+
+% Cria a string de sublinhados
+cria_string_sublinhados(0, []).
+cria_string_sublinhados(N, ['_'|Resto]) :-
+    N > 0,
+    N1 is N - 1,
+    cria_string_sublinhados(N1, Resto).
+
+% Atualiza a forca de acordo com o número de erros
+atualiza_forca(0) :-
+    writeln("      ________    "),
+    writeln("     |/       |   "),
+    writeln("     |        §   "),
+    writeln("     |            "),
+    writeln("     |            "),
+    writeln("     |            "),
+    writeln("   __|            "),
+    writeln("  |  |            "),
+    writeln("  ====            ").
+
+atualiza_forca(1) :-
+    writeln("      ________     "),
+    writeln("     |/       |    "),
+    writeln("     |        §    "),
+    writeln("     |      (*.*)  "),
+    writeln("     |             "),
+    writeln("     |             "),
+    writeln("   __|             "),
+    writeln("  |  |             "),
+    writeln("  ====             ").
+
+atualiza_forca(2) :-
+    writeln("      ________     "),
+    writeln("     |/       |    "),
+    writeln("     |        §    "),
+    writeln("     |      (*.*)  "),
+    writeln("     |        |    "),
+    writeln("     |       [ ]   "),
+    writeln("   __|        |    "),
+    writeln("  |  |             "),
+    writeln("  ====             ").
+
+atualiza_forca(3) :-
+    writeln("      ________     "),
+    writeln("     |/       |    "),
+    writeln("     |        §    "),
+    writeln("     |      (*.*)  "),
+    writeln("     |        |    "),
+    writeln("     |       [ ]   "),
+    writeln("   __|        |    "),
+    writeln("  |  |       /     "),
+    writeln("  ====             ").
+
+atualiza_forca(4) :-
+    writeln("      ________     "),
+    writeln("     |/       |    "),
+    writeln("     |        §    "),
+    writeln("     |      (*.*)  "),
+    writeln("     |        |    "),
+    writeln("     |       [ ]   "),
+    writeln("   __|        |    "),
+    writeln("  |  |       / \\  "),
+    writeln("  ====             ").
+
+atualiza_forca(5) :-
+    writeln("      ________     "),
+    writeln("     |/       |    "),
+    writeln("     |        §    "),
+    writeln("     |      (*.*)  "),
+    writeln("     |        |    "),
+    writeln("     |      /[ ]   "),
+    writeln("   __|        |    "),
+    writeln("  |  |       / \\  "),
+    writeln("  ====             ").
+
+atualiza_forca(6) :-
+    writeln("      ________     "),
+    writeln("     |/       |    "),
+    writeln("     |        §    "),
+    writeln("     |      (*.*)  "),
+    writeln("     |        |    "),
+    writeln("     |      /[ ]\\ "),
+    writeln("   __|        |    "),
+    writeln("  |  |       / \\  "),
+    writeln("  ====             "),
+    writeln("Você perdeu!").
+
+cenaVitoria(Palavra) :-[
+     "  ██████╗███████╗██████╗ ████████╗ █████╗     ██████╗ ███████╗███████╗██████╗  ██████╗ ███████╗████████╗ █████╗ ██╗"
+    ," ██╔════╝██╔════╝██╔══██╗╚══██╔══╝██╔══██╗    ██╔══██╗██╔════╝██╔════╝██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝██╔══██╗██║"
+    ," ██║     █████╗  ██████╔╝   ██║   ███████║    ██████╔╝█████╗  ███████╗██████╔╝██║   ██║███████╗   ██║   ███████║██║"
+    ," ██║     ██╔══╝  ██╔══██╗   ██║   ██╔══██║    ██╔══██╗██╔══╝  ╚════██║██╔═══╝ ██║   ██║╚════██║   ██║   ██╔══██║╚═╝"
+    ," ╚██████╗███████╗██║  ██║   ██║   ██║  ██║    ██║  ██║███████╗███████║██║     ╚██████╔╝███████║   ██║   ██║  ██║██╗"
+    ,"  ╚═════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝      ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝"
+    ,"                               PARABÉNS, VOCÊ VENCEU! A PALAVRA ERA: ", Palavra, "!"].
--- click-jogos-prolog/app/forca.pl
+++ click-jogos-prolog/app/forca.pl
@@ -1,170 +1,63 @@
-:- module(forca, [forca/0]).
-:- discontiguous forca:menu_forca/1.
+:- module(enforcado, [
+    forca/0,
+    get_Entrada/3,
+    eh_valido/2,
+    jogo/2,
+    loop/6,
+    letra_validada/9,
+    cria_string_sublinhados/2,
+    cria_mapa_letras/2,
+    atualiza_string_sublinhados/4,
+    atualiza_forca/2,
+    loop_get_campo_valido/3,
+    campo_valido/1,
+    cenario_perda/1,
+    cenario_vitoria/1
+]).
 
 :- use_module(utils).
 :- use_module(library(lists)).
-:- use_module(library(system)).
-:- use_module(library(charsio)).
+:- use_module(library(apply)).
+:- use_module(library(clpfd)).
+:- use_module(library(pio)). % Para uso de read_line_to_string
 
-% Regra inicial, que realiza o disparo do jogo forca
-forca :-
+forca :- 
+    inicio.
+
+inicio :-
     limpa_terminal,
-    write_text(home_forca(_)),
-    read_line_to_string(user_input, Opcao),
-    menu_forca(Opcao).
+    writeln("                                               "),
+    writeln("   ███████╗ ██████╗ ██████╗  ██████╗ █████╗    "),
+    writeln("   ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗   "),
+    writeln("   █████╗  ██║   ██║██████╔╝██║     ███████║   "),
+    writeln("   ██╔══╝  ██║   ██║██╔══██╗██║     ██╔══██║   "),
+    writeln("   ██║     ╚██████╔╝██║  ██║╚██████╗██║  ██║   "),
+    writeln("   ╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   "),
+    writeln("               SEJA BEM VINDO!                 "),
+    writeln("                                               "),
+    writeln("                 (1) JOGAR                     "),
+    writeln("              (2) SAIR DO JOGO                 "),
+    writeln("                                               "),
+    read_line_to_string(user_input, _opcao),
+    processa_opcao(_opcao).
 
-% Menu de opções do jogo forca
-menu_forca("1") :- processa_dados_partida.
-menu_forca("2") :-
-    sair.
-menu_forca(_) :-
-    colore_amarelo("Opção inválida! Tente novamente. \n", Result),
-    write(Result),
-    forca.
+processa_opcao("1") :- get_dados_partida.
+processa_opcao("2") :- sair.
+processa_opcao(_) :- inicio.
 
-% Processa dados da interação inicial com os usuários
-processa_dados_partida :-
-    regras_do_jogo(_, RegrasEstilizadas),
-    write(RegrasEstilizadas),
-    write("Digite o seu nome Jogador 1: \n"),
+get_dados_partida :-
+    regras_do_jogo(_, Regras),
+    write(Regras),
+    write("Digite o seu nome, Jogador 1: \n"),
     read_line_to_string(user_input, Jogador1),
-    write("Digite o seu nome Jogador 2: \n"),
+    write("Digite o seu nome, Jogador 2: \n"),
     read_line_to_string(user_input, _),
-    printf("\nCerto %s, qual a palavra a ser adivinhada?\n", Jogador1),
-    loop_get_campo_valido("palavra", "Palavra inválida!", Palavra),
-    write("Qual o tema que está relacionado à palavra a ser adivinhada? \n"),
-    loop_get_campo_valido("tema", "Tema inválido!", Tema),
+    format("\nCerto ~s, qual a palavra a ser adivinhada?\n", [Jogador1]),
+    get_Entrada(_, Palavra, "Palavra inválida!"),
+    write("Qual o tema da palavra?\n"),
+    get_Entrada(_, Tema, "Tema inválido!"),
     jogo(Palavra, Tema).
 
-% Lógica principal do jogo
-jogo(Palavra, Tema) :-
-    string_lower(Palavra, PalavraMin),
-    cria_mapa_letras(PalavraMin, MapaLetras),
-    cria_string_sublinhados(Palavra, Sublinhados),
-    loop(Sublinhados, 0, "", MapaLetras, Palavra, Tema).
-
-loop(Sublinhados, Erros, LetrasDigitadas, MapaLetras, Palavra, Tema) :-
-    limpa_terminal,
-    atualiza_forca(Erros, Forca),
-    writeln(Forca),
-    colore_amarelo("\nTEMA: ", TemaAmarelo),
-    bold(TemaAmarelo, TemaBold),
-    printf("%s%s\n", TemaBold, Tema),
-    writeln(Sublinhados),
-    printf("\nLetras digitadas: %s\n", LetrasDigitadas),
-    write("\nDigite uma letra:"),
-    loop_get_campo_valido("letra", "Letra inválida!", LetraDigitada),
-    string_lower(LetraDigitada, LetraMin),
-    string_chars(LetraMin, [Letra]),
-    (   sub_string(LetrasDigitadas, _, _, _, Letra)
-    ->  colore_amarelo("\nEssa letra já foi digitada!\n", Msg),
-        write(Msg),
-        sleep(0.9),
-        loop(Sublinhados, Erros, LetrasDigitadas, MapaLetras, Palavra, Tema)
-    ;   string_concat(Letra, " ", LetrasDigitadas, LetrasDigitadasAtualizada),
-        (   get_dict(Letra, MapaLetras, Indices)
-        ->  atualiza_string_sublinhados(Letra, Sublinhados, Indices, NovoEstadoStringSublinhados),
-            (   string_lower(NovoEstadoStringSublinhados, _)
-            ->  cenario_vitoria(Palavra, CenarioVitoria),
-                write_text(CenarioVitoria),
-                sleep(4),
-                forca
-            ;   loop(NovoEstadoStringSublinhados, Erros, LetrasDigitadasAtualizada, MapaLetras, Palavra, Tema)
-            )
-        ;   (   Erros + 1 >= 6 ->  cenario_perda(Palavra, CenarioPerda),
-                write_text(CenarioPerda),
-                sleep(3),
-                forca
-            ;   loop(Sublinhados, Erros + 1, LetrasDigitadasAtualizada, MapaLetras, Palavra, Tema)
-            )
-        )
-    ).
-
-% Função que cria a string com sublinhados
-cria_string_sublinhados(Palavra, Sublinhados) :-
-    string_length(Palavra, Length),
-    length(SublinhadosList, Length), % cria uma lista de caracteres do mesmo tamanho que a palavra.
-    maplist(=('_'), SublinhadosList), % a cada posição da lista, adiciona um caractere '_' (whitespace).
-    string_chars(Sublinhados, SublinhadosList). % converte a sublinhadosList para string sublinados.
-
-% Função que cria um mapa de letra -> posições
-cria_mapa_letras(Palavra, MapaLetras) :-
-    string_chars(Palavra, Chars),
-    findall(Letra-Pos, nth0(Pos, Chars, Letra), Pairs),
-    group_pairs_by_key(Pairs, GroupedPairs),
-    dict_create(MapaLetras, _, GroupedPairs).
-
-% Função para atualizar a string de sublinhados com a letra correta
-atualiza_string_sublinhados(_, [], _, []).
-atualiza_string_sublinhados(Letra, [_|T], Indices, [Letra|NovoT]) :-
-    member(0, Indices), !,
-    maplist(dec, Indices, NovosIndices),
-    atualiza_string_sublinhados(Letra, T, NovosIndices, NovoT).
-atualiza_string_sublinhados(Letra, [H|T], Indices, [H|NovoT]) :-
-    maplist(dec, Indices, NovosIndices),
-    atualiza_string_sublinhados(Letra, T, NovosIndices, NovoT).
-
-dec(X, Y) :- Y is X - 1.
-
-% Função que requer em loop campo, caso não validado corretamente
-loop_get_campo_valido(Campo, Mensagem, Input) :-
-    get_input(Campo, InputTemp),
-    (   is_valid(Campo, InputTemp)
-    ->  Input = InputTemp
-    ;   colore_amarelo(Mensagem, Msg),
-        bold(Msg, BoldMsg),
-        writeln(BoldMsg),
-        loop_get_campo_valido(Campo, Mensagem, Input)
-    ).
-
-% Define formato em receber input, caso palavra ele esconde o input com a função hide_input 
-get_input("tema", Input) :- read_line_to_string(user_input, Input).
-get_input("letra", Input) :- read_line_to_string(user_input, Input).
-get_input("palavra", Input) :- hide_input(Input).
-
-% Realiza validação de acordo com o tipo de campo
-is_valid("tema", Input) :- campo_valido(Input), string_length(Input, Length), Length > 1.
-is_valid("letra", Input) :- campo_valido(Input), string_length(Input, 1).
-is_valid("palavra", Input) :- campo_valido(Input), string_length(Input, Length), Length > 1, \+ sub_string(Input, _, _, _, ' ').
-
-% Função para ocultar a entrada do usuário e avançar o prompt a cada letra digitada
-hide_input(Input) :-
-    %set_prolog_flag(tty_control, true),
-    get_single_char(Char),
-    hide_input_loop(Char, [], Input).
-
-hide_input_loop(Char, ListaLetras, Input) :-
-    put_char('*'),
-    flush_output, % garanter que o asterisco seja imediatamente exibido no console.
-    get_single_char(NextChar), % lê o próximo caracter digitado pelo usuário e unifica com o NextChar.
-    hide_input_loop(NextChar, [Char|ListaLetras], Input). % chamada recursiva acumulando o caractere atual na ListaLetras.
-
-hide_input_loop(10, ListaLetras, Input) :- !,
-    reverse(ListaLetras, RevListaLetras),
-    string_chars(Input, RevListaLetras),
-    nl.
-
-% Escreve texto
-write_text(Text):-
-    maplist(writeln, Text).
-
-% Desenha a forca atualizada a cada erro (contado) passado
-atualiza_forca(0, Forca) :-
-    Forca = "      ________    \n     |/       |   \n     |        §   \n     |            \n     |            \n     |            \n   __|            \n  |  |            \n  ====            ".
-atualiza_forca(1, Forca) :-
-    Forca = "      ________     \n     |/       |    \n     |        §    \n     |      (*.*)  \n     |             \n     |             \n   __|             \n  |  |             \n  ====             ".
-atualiza_forca(2, Forca) :-
-    Forca = "      ________     \n     |/       |    \n     |        §    \n     |      (*.*)  \n     |        |    \n     |       [ ]   \n   __|        |    \n  |  |             \n  ====             ".
-atualiza_forca(3, Forca) :-
-    Forca = "      ________     \n     |/       |    \n     |        §    \n     |      (*.*)  \n     |        |    \n     |       [ ]   \n   __|        |    \n  |  |       /     \n  ====             ".
-atualiza_forca(4, Forca) :-
-    Forca = "      ________     \n     |/       |    \n     |        §    \n     |      (*.*)  \n     |        |    \n     |       [ ]   \n   __|        |    \n  |  |       / \\  \n  ====             ".
-atualiza_forca(5, Forca) :-
-    Forca = "      ________     \n     |/       |    \n     |        §    \n     |      (*.*)  \n     |        |    \n     |      /[ ]   \n   __|        |    \n  |  |       / \\  \n  ====             ".
-atualiza_forca(6, Forca) :-
-    Forca = "      ________     \n     |/       |    \n     |        §    \n     |      (*.*)  \n     |        |    \n     |      /[ ]\\ \n   __|        |    \n  |  |       / \\  \n  ====             ".
-
-% Regras do jogo
 regras_do_jogo(Regras, RegrasEstilizadas) :-
     Regras = "\n📜 Regras do jogo: \n- O jogador 1 será o jogador que dirá a palavra para ser adivinhada, assim como qual tema ela se relaciona.
                \n- O jogador 2 será o jogador que tentará adivinhar a palavra dada pelo jogador 1.
@@ -175,49 +68,196 @@ regras_do_jogo(Regras, RegrasEstilizadas) :-
     bold(RegrasAmarelas, RegrasBold),
     RegrasEstilizadas = RegrasBold.
 
-% Tela inicial do jogo
-menu_forca(TelaInicial) :-
-    TelaInicial = [
-        "                                               ",
-        "   ███████╗ ██████╗ ██████╗  ██████╗ █████╗    ",
-        "   ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗   ",
-        "   █████╗  ██║   ██║██████╔╝██║     ███████║   ",
-        "   ██╔══╝  ██║   ██║██╔══██╗██║     ██╔══██║   ",
-        "   ██║     ╚██████╔╝██║  ██║╚██████╗██║  ██║   ",
-        "   ╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ",
-        "               SEJA BEM VINDO!                 ",
-        "                                               ",
-        "                 (1) JOGAR                     ",
-        "              (2) SAIR DO JOGO                 ",
-        "                                               "
-    ].
+get_Entrada(_, Entrada, MsgErro) :- 
+    read_line_to_string(user_input, EntradaTemp),
+    trim(EntradaTemp, EntradaTrimmed),
+    (eh_valido(_, EntradaTrimmed) -> 
+        Entrada = EntradaTrimmed
+        ; writeln(MsgErro), get_Entrada(_, Entrada, MsgErro)).
 
-% Cenário de perda
-cenario_perda(Palavra, CenarioPerda) :-
-    string_upper(Palavra, PalavraUpper),
-    bold(PalavraUpper, PalavraBold),
-    string_concat("                           A PALAVRA ERA: ", PalavraBold, Mensagem),
-    CenarioPerda = [
-        " ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ ",
-        "██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗",
-        "██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝",
-        "██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗",
-        "╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║",
-        " ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝",
-        Mensagem
-    ].
+eh_valido(_, Texto) :- 
+    string(Texto),
+    string_length(Texto, Tamanho),
+    Tamanho > 0,
+    trim(Texto, TextoTrim),
+    TextoTrim \= "". 
 
-% Cenário de vitória
-cenario_vitoria(Palavra, CenarioVitoria) :-
-    string_upper(Palavra, PalavraUpper),
-    bold(PalavraUpper, PalavraBold),
-    string_concat("                               PARABÉNS, VOCÊ VENCEU! A PALAVRA ERA: ", PalavraBold, Mensagem)
-    CenarioVitoria = [
-        "  ██████╗███████╗██████╗ ████████╗ █████╗     ██████╗ ███████╗███████╗██████╗  ██████╗ ███████╗████████╗ █████╗ ██╗",
-        " ██╔════╝██╔════╝██╔══██╗╚══██╔══╝██╔══██╗    ██╔══██╗██╔════╝██╔════╝██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝██╔══██╗██║",
-        " ██║     █████╗  ██████╔╝   ██║   ███████║    ██████╔╝█████╗  ███████╗██████╔╝██║   ██║███████╗   ██║   ███████║██║",
-        " ██║     ██╔══╝  ██╔══██╗   ██║   ██╔══██║    ██╔══██╗██╔══╝  ╚════██║██╔═══╝ ██║   ██║╚════██║   ██║   ██╔══██║╚═╝",
-        " ╚██████╗███████╗██║  ██║   ██║   ██║  ██║    ██║  ██║███████╗███████║██║     ╚██████╔╝███████║   ██║   ██║  ██║██╗",
-        "  ╚═════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝      ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝",
-        Mensagem
-    ].
+eh_valido("letra", Tema) :- 
+    string(Tema),
+    string_length(Tema, Tamanho),
+    Tamanho > 0,
+    Tamanho == 1.
+
+% Lógica principal do jogo
+jogo(Palavra, Tema) :-
+    cria_mapa_letras(Palavra, MapaLetras),  % Mapa de letras em minúsculas
+    cria_string_sublinhados(Palavra, EstadoAtual), % cria string sublinhada com o tamanho da palavra
+
+    loop(EstadoAtual, 0, [], MapaLetras, Tema, Palavra).
+
+loop(StringSublinhada, Erros, LetrasDigitadas, MapaLetras, Tema, Palavra) :-
+    limpa_terminal,
+    atualiza_forca(Erros, ForcaAtual),
+    write(ForcaAtual), nl,
+
+    format("~sTEMA: ~s~n", ["\27[33m", Tema]),  % Colore o tema
+    writeln(StringSublinhada),
+    format("\nLetras digitadas: ~s~n", [LetrasDigitadas]),
+    write("Digite uma letra: "),
+    read_letra(LetraDigitada),
+
+    letra_validada(LetraDigitada, LetrasDigitadas, StringSublinhada, Erros, MapaLetras, Palavra, NovoEstadoStringSublinhados, NovoErros, LetrasDigitadasAtualizada),
+    (   NovoErros >= 6 -> 
+        cenario_perda(Palavra);
+        loop(NovoEstadoStringSublinhados, NovoErros, LetrasDigitadasAtualizada, MapaLetras, Tema, Palavra)
+    ).
+
+letra_validada(LetraDigitada, LetrasDigitadas, StringSublinhada, Erros, MapaLetras, Palavra, NovoEstadoStringSublinhados, NovoErros, LetrasDigitadasAtualizada) :-
+    downcase_atom(LetraDigitada, Letra),
+    (   member(Letra, LetrasDigitadas) -> 
+        writeln("\27[33mEssa letra já foi digitada!\27[0m"), % Mensagem se a letra já foi digitada
+        sleep(1),
+        (Erros =< 5 -> NovoErros = Erros; NovoErros = Erros);
+        % Atualiza letras digitadas
+        atom_concat(" ", Letra, LetrasDigitadasAtualizada),
+        (   get_assoc(Letra, MapaLetras, Indices) -> 
+            atualiza_string_sublinhados(Letra, StringSublinhada, Indices, NovoEstadoStringSublinhados),
+            (   downcase_atom(NovoEstadoStringSublinhados, Palavra) -> 
+                cenario_vitoria(Palavra);
+                NovoErros = Erros
+            )
+        ;   
+            NovoErros is Erros + 1
+        )
+    ).
+
+% Função que cria a string com sublinhados
+cria_string_sublinhados(Palavra, EstadoSublinhado) :-
+    string_length(Palavra, Tamanho),
+    atom_chars(EstadoSublinhado, "_"),
+    length(EstadoSublinhado, Tamanho).
+
+% Função que cria um mapa de letra -> posições
+cria_mapa_letras(Palavra, Mapa) :-
+    string_chars(Palavra, Letras),
+    findall((Letra, [I]), (nth0(I, Letras, Letra)), ListaAssociativa),
+    group_pairs_by_key(ListaAssociativa, Mapa).
+
+% Função para atualizar a string de sublinhados com a letra correta
+atualiza_string_sublinhados(Letra, Sublinhados, Indices, NovoEstado) :-
+    atom_chars(Sublinhados, CharsSublinhados),
+    findall(NovoChar, (between(0, 20, I), (member(I, Indices) -> NovoChar = Letra ; nth0(I, CharsSublinhados, NovoChar))), NovoChars),
+    atom_chars(NovoEstado, NovoChars).
+
+% Desenha a forca atualizada a cada erro (contado) passado
+atualiza_forca(0, Forca) :- 
+    Forca = 
+        "      ________    \n" +
+        "     |/       |   \n" +
+        "     |        §   \n" +
+        "     |            \n" +
+        "     |            \n" +
+        "     |            \n" +
+        "   __|            \n" +
+        "  |  |            \n" +
+        "  ====            \n".
+
+atualiza_forca(1, Forca) :- 
+    Forca = 
+        "      ________     \n" +
+        "     |/       |    \n" +
+        "     |        §    \n" +
+        "     |      (*.*)  \n" +
+        "     |             \n" +
+        "     |             \n" +
+        "   __|             \n" +
+        "  |  |             \n" +
+        "  ====             \n".
+
+atualiza_forca(2, Forca) :- 
+    Forca = 
+        "      ________     \n" +
+        "     |/       |    \n" +
+        "     |        §    \n" +
+        "     |      (*.*)  \n" +
+        "     |        |    \n" +
+        "     |       [ ]   \n" +
+        "   __|        |    \n" +
+        "  |  |             \n" +
+        "  ====             \n".
+
+atualiza_forca(3, Forca) :- 
+    Forca = 
+        "      ________     \n" +
+        "     |/       |    \n" +
+        "     |        §    \n" +
+        "     |      (*.*)  \n" +
+        "     |        |    \n" +
+        "     |       [ ]   \n" +
+        "   __|        |    \n" +
+        "  |  |       /     \n" +
+        "  ====             \n".
+
+atualiza_forca(4, Forca) :- 
+    Forca = 
+        "      ________     \n" +
+        "     |/       |    \n" +
+        "     |        §    \n" +
+        "     |      (*.*)  \n" +
+        "     |        |    \n" +
+        "     |       [ ]   \n" +
+        "   __|        |    \n" +
+        "  |  |       / \\  \n" +
+        "  ====             \n".
+
+atualiza_forca(5, Forca) :- 
+    Forca = 
+        "      ________     \n" +
+        "     |/       |    \n" +
+        "     |        §    \n" +
+        "     |      (*.*)  \n" +
+        "     |        |    \n" +
+        "     |      /[ ]   \n" +
+        "   __|        |    \n" +
+        "  |  |       / \\  \n" +
+        "  ====             \n".
+
+atualiza_forca(6, Forca) :- 
+    Forca = 
+        "      ________     \n" +
+        "     |/       |    \n" +
+        "     |        §    \n" +
+        "     |      (*.*)  \n" +
+        "     |        |    \n" +
+        "     |      /[ ]\\ \n" +
+        "   __|        |    \n" +
+        "  |  |       / \\  \n" +
+        "  ====             \n".
+
+% Função que requer em loop campo, caso não validado corretamente
+loop_get_campo_valido(Campo, Mensagem, Resposta) :-
+    read_line_to_string(user_input, Input),
+    (   campo_valido(Input) -> 
+        Resposta = Input;
+        writeln(Mensagem),
+        loop_get_campo_valido(Campo, Mensagem, Resposta)
+    ).
+
+campo_valido(Campo) :- 
+    \+ (Campo == ""), \+ (all_chars_space(Campo)), \+ (any_char_digit(Campo)).
+
+all_chars_space(String) :- 
+    string_chars(String, Chars), 
+    forall(member(Char, Chars), char_type(Char, space)).
+
+any_char_digit(String) :- 
+    string_chars(String, Chars), 
+    member(Char, Chars), 
+    char_type(Char, digit).
+
+cenario_perda(Palavra) :-
+    format("A PALAVRA ERA: ~s!\n", [Palavra]),
+    writeln("Você perdeu!").
+
+cenario_vitoria(Palavra) :-
+    format("PARABÉNS, VOCÊ VENCEU! A PALAVRA ERA: ~s!\n", [Palavra]).
