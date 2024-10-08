67b98547a7555115c0c54dae29a7e83b79467f17
luisa <luisa.azevedo.ext@synchro.com.br>
2024-10-08 00:40:28 -0300

    Corrige configuração utf8

--- click-jogos-prolog/app/main.pl
+++ click-jogos-prolog/app/main.pl
@@ -3,10 +3,9 @@
 :- initialization(configura_unicode).
 
 configura_unicode :-
-    % Configura a codificação de entrada e saída como UTF-8
-    set_stream(user_output, encoding(utf8)),
-    set_stream(user_input, encoding(utf8)),
-    % Se necessário, ajuste a codificação padrão do Prolog
-    set_prolog_flag(encoding, utf8).
+    set_prolog_flag(encoding, utf8),
+    open('output.txt', write, Stream, [encoding(utf8)]),
+    set_stream(user_output, Stream),
+    set_stream(user_input, encoding(utf8)).
 
 :- inicio.
