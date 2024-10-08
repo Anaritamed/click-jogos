6591b54316848ab5cd7db425a334d3ab22c27efd
luisa <luisa.azevedo.ext@synchro.com.br>
2024-10-08 00:27:58 -0300

    Adiciona configuração para exibir corretamente caracteres unicode

--- click-jogos-prolog/app/main.pl
+++ click-jogos-prolog/app/main.pl
@@ -1,2 +1,12 @@
 :- use_module(menu_inicial).
+
+:- initialization(configura_unicode).
+
+configura_unicode :-
+    % Configura a codificação de entrada e saída como UTF-8
+    set_stream(user_output, encoding(utf8)),
+    set_stream(user_input, encoding(utf8)),
+    % Se necessário, ajuste a codificação padrão do Prolog
+    set_prolog_flag(encoding, utf8).
+
 :- inicio.
